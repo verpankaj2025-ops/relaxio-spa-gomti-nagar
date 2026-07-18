7fc53e962367d20c5a538f7cb7faf1cf8f618afa
pankajverma <YOUR_GMAIL@gmail.com>
2026-07-18 07:28:56 +0530

    Install Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,20 +106,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en-IN" className={`${inter.variable} ${playfair.variable}`}>
       <body className="font-sans bg-[#fdfbf7] text-stone-900 antialiased selection:bg-[#d4af37]/30" suppressHydrationWarning>
 
-      <Script
-  src="https://www.googletagmanager.com/gtag/js?id=AW-17470206378"
-  strategy="afterInteractive"
-/>
+     <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-TZT2H3DC"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
 
-<Script id="google-ads-tag" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    window.gtag = gtag;
-
-    gtag('js', new Date());
-    gtag('config', 'AW-17470206378');
-  `}
+<Script id="gtm" strategy="afterInteractive">
+{`
+(function(w,d,s,l,i){
+  w[l]=w[l]||[];
+  w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),
+      dl=l!='dataLayer'?'&l='+l:'';
+  j.async=true;
+  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+  f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZT2H3DC');
+`}
 </Script>
 
         <script
